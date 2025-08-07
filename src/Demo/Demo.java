a6f7d0cf6c47f252081b93461a7d6a4641f494db
pasindupramod755 <pasidubandara12345@gmail.com>
2025-08-07 22:23:25 +0530

    initial commit

---
+++ src/Demo/Demo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Demo;
+
+/**
+ *
+ * @author Pasindu Bandara
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Final CorseWork");
+    }
+}
